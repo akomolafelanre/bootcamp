6a9d9d0638a8e69856eba916966dbc3a17087ca6
Lanre Akomolafe <akomolafelanre@gmail.com>
2015-07-07 19:00:08 +0100

    Implementation of problem_3 to check if a list of password is valid

---
+++ problem_3.py
@@ -0,0 +1,41 @@
+def check_lowercase(word):
+	i = 0
+	word = list(word)
+	for item in word:
+		if item.islower():
+			i += 1
+	return i
+
+def check_uppercase(word):
+	i = 0
+	word = list(word)
+	for item in word:
+		if item.isupper():
+			i += 1
+	return i
+
+def check_numbers(word):
+	i = 0
+	word = list(word)
+	for item in word:
+		if item.isdigit():
+			i += 1
+	return i
+
+def check_passwords(s):
+	result = 0
+	lowercase = 0
+	uppercase = 0
+	numbers = 0
+
+	passwords = open(s, 'r')
+	for item in passwords:
+		lowercase = check_lowercase(item)
+		uppercase = check_uppercase(item)
+		numbers = check_numbers(item)
+		if (numbers >= 4) and (lowercase >= 2) and (uppercase >= 4):
+			result += 1
+
+	print "%s has %d valid passwords." %(s, result)
+
+check_passwords("password.txt")
