07372040f6f159ca2735a5300d9fc3724099e550
Lanre Akomolafe <akomolafelanre@gmail.com>
2015-07-07 17:58:27 +0100

    Modification of problem_2 to jump delay on the last url

--- problem_2.py
+++ problem_2.py
@@ -4,10 +4,13 @@ import time
 def open_websites(web_list):
 	site =""
 	tim = 0
-	for item in web_list:
-		site = item[0]
-		tim = item[1]
-		webbrowser.open(site)
-		time.sleep(tim)
+	for i in range(0,len(web_list)):
+		site = web_list[i][0]
+		tim = web_list[i][1]
+		if i == len(web_list)-1:
+			webbrowser.open(site)
+		else:
+			webbrowser.open(site)
+			time.sleep(tim)
 
-open_websites([["www.google.com", 10], ["www.google.com", 10]])
+open_websites([["www.google.com", 10], ["www.bing.com", 10], ["www.google.com", 10]])
