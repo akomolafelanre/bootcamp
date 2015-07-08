a7b2baa08b2cd8adbd21248ec7768d827bbcb9b7
Lanre Akomolafe <akomolafelanre@gmail.com>
2015-07-09 00:17:03 +0100

    Implementation of the fibonnaci sequence challenge and the test platform

--- problem_4.py
+++ problem_4.py
@@ -1,3 +1,8 @@
+def generateNextValue(n):
+	a = len(n)-1
+	b = n[a] + n[a-1]
+	return b
+
 def generateSeries(n):
 	if (type(n) != int):
 		return 'Not an Integer'
@@ -11,12 +16,41 @@ def generateSeries(n):
 		sequence.append(sum)
 	return sequence
 
-
-
-
-
 def calculateSum(n):
-	pass	
+	if (type(n) != list):
+		return 'Not a list'
+	if len(n) == 0:
+		return 'The list is empty'
+	for item in n:
+		if type(item) != int:
+			return 'The list contains a non integer value'
+	return sum(n)
 
-def generateSeriesSum(n, key= None):
-	pass
+def generateSeriesSum(n, filt= None):
+	if (type(n) != int):
+		return 'Not an Integer'
+	if (n < 2):
+		return 'The number provided must be an integer geater than 1'
+	key = filt
+	initial_array = [0,1]
+	final_array = []
+	if key == None:
+		initial_array = generateSeries(n)
+		return calculateSum(initial_array)
+	if key == 'even':
+		while len(final_array) < n:
+			temp = generateNextValue(initial_array)
+			initial_array.append(temp)
+			if temp % 2 == 0:
+				final_array.append(temp)
+		return calculateSum(final_array)
+	if key == 'odd':
+		final_array = [1]
+		while len(final_array) < n:
+			temp = generateNextValue(initial_array)
+			initial_array.append(temp)
+			if temp % 2 == 1:
+				final_array.append(temp)
+		return calculateSum(final_array)
+	if key != 'odd' and key != 'even':
+		return "This function can only be called with 'odd' or 'even'"
--- problem_4_test.py
+++ problem_4_test.py
@@ -2,6 +2,16 @@ import unittest, problem_4
 
 class TestFunWithFibonacci(unittest.TestCase):
 	
+	def testGNextValue_CheckCorrectResult(self):
+		result = problem_4.generateNextValue([0,1,1])
+		self.assertEqual(result, 2,  \
+			msg = "This should return an empty list")
+
+	def testGNextValue_CheckCorrectResult2(self):
+		result = problem_4.generateNextValue([0,1,1,2,3])
+		self.assertEqual(result, 5,  \
+			msg = "This should return an empty list")
+
 	def testGSeries_NotInteger(self):
 		result = problem_4.generateSeries("A")
 		self.assertEqual(result, 'Not an Integer',  \
@@ -62,6 +72,16 @@ class TestFunWithFibonacci(unittest.TestCase):
 		self.assertEqual(result, 13,  \
 			msg = "This should return the sum of [3, 4, 5, 1] as 13 ")
 
+	def testGSeriesSum_IsInputCorrect(self):
+		result = problem_4.generateSeriesSum("A")
+		self.assertEqual(result, 'Not an Integer',  \
+			msg = "This should return that the input is not an integer value")
+
+	def testGSeriesSum_IsInputCorrect2(self):
+		result = problem_4.generateSeriesSum([1,2])
+		self.assertEqual(result, 'Not an Integer',  \
+			msg = "This should return that the input is not an integer value")
+
 	def testGSeriesSum_IsResultCorrect(self):
 		result = problem_4.generateSeriesSum(3)
 		self.assertEqual(result, 2,  \
@@ -69,32 +89,37 @@ class TestFunWithFibonacci(unittest.TestCase):
 
 	def testGSeriesSum_IsResultCorrect2(self):
 		result = problem_4.generateSeriesSum(1)
-		self.assertEqual(result, 0,  \
-			msg = "This should return the sum of fibonacci sequence with 1 elements as 0")
+		self.assertEqual(result, 'The number provided must be an integer geater than 1',  \
+			msg = "This should return an error string since a fibbonnacci sequence must have at least 2 elements")
 
 	def testGSeriesSum_IsResultCorrect3(self):
-		result = problem_4.generateSeriesSum(5, 'even')
-		self.assertEqual(result, 2,  \
+		result = problem_4.generateSeriesSum(4, 'even')
+		self.assertEqual(result, 188,  \
 			msg = "This should return the sum of even numbers in a fibonacci sequence with 5 elements as 2")
 
 	def testGSeriesSum_IsResultCorrect4(self):
 		result = problem_4.generateSeriesSum(3, 'even')
-		self.assertEqual(result, 0,  \
+		self.assertEqual(result, 44,  \
 			msg = "This should return the sum of even numbers in a fibonacci sequence with 3 elements as 0")
 
 	def testGSeriesSum_IsResultCorrect5(self):
-		result = problem_4.generateSeriesSum(5, 'odd')
-		self.assertEqual(result, 5,  \
+		result = problem_4.generateSeriesSum(4, 'odd')
+		self.assertEqual(result, 10,  \
 			msg = "This should return the sum of odd numbers in a fibonacci sequence with 5 elements as 5")
 
 	def testGSeriesSum_IsResultCorrect6(self):
 		result = problem_4.generateSeriesSum(2, 'odd')
-		self.assertEqual(result, 1,  \
+		self.assertEqual(result, 2,  \
 			msg = "This should return the sum of odd numbers in a fibonacci sequence with 2 elements as 1")
 
 	def testGSeriesSum_IsResultCorrect7(self):
-		result = problem_4.generateSeriesSum(1, 'all')
+		result = problem_4.generateSeriesSum(5, 'all')
 		self.assertEqual(result, "This function can only be called with 'odd' or 'even'",  \
+			msg = "This should return that function is called with a wrong parameter")
+
+	def testGSeriesSum_IsResultCorrect8(self):
+		result = problem_4.generateSeriesSum(1, 'all')
+		self.assertEqual(result, 'The number provided must be an integer geater than 1',  \
 			msg = "This should return that function is called with a wrong parameter")
 
 
